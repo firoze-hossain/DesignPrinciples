f7fe04a5ca806c1306b20614f0ed5d0a0f1dc95e
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-02-16 09:49:55 +0600

    the garbage code

--- src/com/roze/solid/singleResponsibilityPrinciple/reminder/App.java
+++ src/com/roze/solid/singleResponsibilityPrinciple/reminder/App.java
@@ -4,9 +4,22 @@ import java.time.LocalDateTime;
 
 public class App {
     public static void main(String[] args) {
-        Reminder reminder = new Reminder(101, "Play Cricket",
+        Reminder reminder = new Reminder();
+        Reminder playCricket = new Reminder(101, "playing cricket",
                 LocalDateTime.of(2024, 02, 17, 10, 15));
-        Reminder reminder1 = new Reminder(102, "Coding",
+        Reminder code = new Reminder(102, "Coding",
                 LocalDateTime.of(2024, 02, 17, 8, 00));
+        Reminder workOut = new Reminder(103, "Work out for 2 hours",
+                LocalDateTime.of(2024, 02, 17, 5, 30));
+
+        reminder.addReminder(playCricket);
+        reminder.addReminder(code);
+        reminder.addReminder(workOut);
+        reminder.printReminders();
+
+//        reminder.removeReminder(1);
+//        System.out.println("*********After removing reminder**********");
+//        reminder.printReminders();
+        reminder.saveReminders("friday");
     }
 }
